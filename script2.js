1eb19a186f431402c5bebea5c35838dcc2e49a99
Jackie <jacgut19@gmail.com>
2017-12-11 16:22:42 +0000

    search button works

--- script2.js
+++ script2.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
                success: function(data){
                    console.log(data);
                    //function show(data{
-                 $('#city').text(data.name + "," + data.sys.country);
-                 $('#weather').text(data.weather[0].main);
+                $('#city').text(data.name + "," + data.sys.country);
+                $('#weather').text(data.weather[0].main);
                 $('#temp').text(data.main.temp);
                 $('#description').text(data.weather[0].description);
                 $('#pressure').text(data.main.pressure);
@@ -20,45 +20,15 @@ $(document).ready(function(){
                 $('#minTemp').text(data.main.temp_min);
                 $('#maxTemp').text(data.main.temp_max);
                 $('#speed').text(data.wind.speed);
-                $('#direct').text(data.wind.deg);
 //}
              }
           });
-//    $('#submitWeather').click(function(){
-//      var city = $("#city").val();
-//      console.log('click');
-//      if(city != ''){
+    $('#submitWeather').click(function(){
+     var city = $("#city").val();
+     console.log('click');
+      if(city != ''){
           
 
         } else $("#error").html('Enter City');
     });
 });
-
-
-
-
-/*$(document).ready(function() {
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            var temp;
-            var TempF;
-            var TempC;
-    $.ajax({
-         method: "GET",
-         url: "https://api.openweathermap.org/data/2.5/weather",
-         data: {lat: lat, lon: long, units:'imperial', APPID:"10bfa7a03dd5e3c5cfc68b4cb722a439"},
-           success: function(data) {
-              temp = data.main.temp;
-              $('#city').text(data.name);
-              $('#temp').text(temp.toFixed(0));
-              $('#description').text(data.weather[0].description);
-              $('#weatherType').text(data.weather[0].main);
-            }
-          });
-        }
-      }
-  });
-     
-  $("#FahrToCel").click(function() {*/
