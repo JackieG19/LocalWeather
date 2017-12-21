d52bfb62c19d38309350be072be5c572c8df3d07
Jackie <jacgut19@gmail.com>
2017-12-21 17:59:12 +0000

    commenting

--- script2.js
+++ script2.js
@@ -10,23 +10,23 @@ $(document).ready(function() {
   
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {      
-    lon = position.coords.longitude;
-    lat = position.coords.latitude;
-    console.log(navigator.geolocation);
+    lon = position.coords.longitude; //var lon
+    lat = position.coords.latitude; // var lat
+    console.log(navigator.geolocation); //checking to see if navigator.geolocation works
       
-    var myUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' +
+    var myUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + 
                   lat + '&lon=' + lon + '&units=metric&appid=' + APIKEY;
                   console.log(myUrl);
       $.ajax({
-          url : myUrl,
-          method : 'GET',
+          url : myUrl,//var myUrl
+          method : 'GET',//request information from the url
           success : function (data) {        
             var temp = data.main.temp;
             var location = data.name;                       
             var imgSrc = data.weather[0].icon;
             tempC = Math.round(temp);
             tempF = Math.round((temp * 1.8) + 32);
-            console.log(temp);
+            console.log(temp);//checking for temperature from the url 
             
             $('#pic').html("<img src=\"http://openweathermap.org/img/w/" + imgSrc + ".png\" width=\"80px\"></img><br>");
             $('#message').html("<h4>" + tempC + 'C ' + location);
@@ -34,13 +34,13 @@ $(document).ready(function() {
       });            
   });
 } else 
-    $('#message').html("<h4>No Location</h4>");
+    $('#message').html("<h4>No Location</h4>"); // displays temperature
     
   // F <----> C //
   $(document).ready(function() {          
-    $("#togBtn").on("click", function(){
+    $("#Btn").on("click", function(){ // button function that changes farenheit to celcius
       
-      var weather = document.getElementById('message').innerHTML;     
+      var weather = document.getElementById('message').innerHTML; //id = message from html
       if(weather.indexOf("C") > 0)                       
         $('#message').html(weather.replace(tempC + "C", tempF + "F"));
       else         
